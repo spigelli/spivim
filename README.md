d61952cb734be25ec6a1f3b2c848f0721da04307
Isaac <isaacspiegel@gmail.com>
2025-03-21 17:50:29 -0500

    Install a bunch of stuff

---
+++ README.md
@@ -0,0 +1,5 @@
+## Deps
+
+- fish
+- fd
+- brew install ghostscript basictex tectonic
--- src/lua/config/options.lua
+++ src/lua/config/options.lua
@@ -1,3 +1,8 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- set g:loaded_perl_provider = 0 but with lua api
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
+vim.g.loaded_julia_provider = 0
